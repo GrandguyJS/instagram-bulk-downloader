2512789d69c13b7f62751f564475ba166affab35
GrandguyJS <GrandguyMC@gmail.com>
2025-05-19 15:37:50 +0200

    bulk download correctly - failed downloads go into txt

--- download_instareels.py
+++ download_instareels.py
@@ -16,24 +16,26 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 def download_reels(links): # Downloads all reels as .mov files
-    for i, link in enumerate(links, start=1):
-        ydl_opts = {
-            "outtmpl": f"{output_dir}/%(id)s.%(ext)s",
-            "format": "bestvideo+bestaudio/best",
-            "postprocessors": [{
-                "key": "FFmpegVideoConvertor",
-                "preferedformat": "mov"
-            }],
-            "quiet": True,
-            "cookiefile": "cookies.txt"
-        }
+    ydl_opts = {
+        "outtmpl": f"{output_dir}/%(id)s1.%(ext)s",
+        "format": "bestvideo+bestaudio/best",
+        "postprocessors": [{
+            "key": "FFmpegVideoConvertor",
+            "preferedformat": "mov"
+        }],
+        "quiet": True,
+        "cookiefile": "cookies.txt"
+    }
 
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([link])
-                print("Downloaded Video #", i)
-        except Exception as e:
-            print(e)
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(links)
+    except Exception as e:
+        print(e)
+    for link in links:
+        if not os.path.exists(os.path.join(output_dir, link.split("/reel/")[1].split("/")[0]+".mov")):
+            with open(os.path.join(os.path.dirname(__file__), "failed.txt"), "a") as f:
+                f.write(link + "\n")
 
 if __name__ == "__main__":
     download_reels(re.findall(r'https?://\S+', reels_text))
